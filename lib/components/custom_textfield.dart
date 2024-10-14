748496f026dfe29788d0e7aa8914af070913faaf
James Jordan Vergara <jjrvergara10@gmail.com>
2024-10-14 19:31:38 +0800

    add validations

--- lib/components/custom_textfield.dart
+++ lib/components/custom_textfield.dart
@@ -3,39 +3,42 @@ import 'package:flutter/material.dart';
 class CustomTextfield extends StatelessWidget {
   final String label;
   final TextEditingController controller;
+  final String? Function(String?)? validator; // Add validator
 
   const CustomTextfield({
     super.key,
     required this.label,
     required this.controller,
+    this.validator,
   });
 
-   @override
+  @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: controller,
+      validator: validator, // Use validator in the form field
       decoration: InputDecoration(
-        labelText: label, // Label text inside the TextField
-        labelStyle: TextStyle(color: Colors.grey[700]), // Label text color
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.grey[700]),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0), // Rounded corners
+          borderRadius: BorderRadius.circular(10.0),
           borderSide: const BorderSide(
-            color: Colors.grey, // Thin border color
-            width: 1.0, // Border thickness
+            color: Colors.grey,
+            width: 1.0,
           ),
         ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10.0),
           borderSide: const BorderSide(
-            color: Colors.grey, // Color of the border when not focused
-            width: 1.0, // Border thickness
+            color: Colors.grey,
+            width: 1.0,
           ),
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10.0),
           borderSide: const BorderSide(
-            color: Colors.deepPurple, // Color of the border when focused
-            width: 1.0, // Border thickness
+            color: Colors.deepPurple,
+            width: 1.0,
           ),
         ),
       ),
--- lib/pages/customer_details_page.dart
+++ lib/pages/customer_details_page.dart
@@ -14,18 +14,12 @@ class CustomerDetailsPage extends StatefulWidget {
 }
 
 class _CustomerDetailsPageState extends State<CustomerDetailsPage> {
-  // final List<String> _venues = ['Seminar Room']; // Venue dropdown
-  // final TextEditingController _firstNameController = TextEditingController();
-  // final TextEditingController _lastNameController = TextEditingController();
-  // final TextEditingController _emailController = TextEditingController();
-  // final TextEditingController _notesController = TextEditingController();
-  //final maxLines = 5;
-
-  //String? _selectedVenue;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     final bookingProvider = Provider.of<BookingProvider>(context);
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -37,139 +31,141 @@ class _CustomerDetailsPageState extends State<CustomerDetailsPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(35.0),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Select a Venue',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(
-                      height: 20), // A little space between label and dropdown
-                  DropdownButtonFormField<String>(
-                    value: bookingProvider.selectedVenue,
-                    decoration: InputDecoration(
-                      labelText: 'Venue', // Add label text inside the dropdown
-                      labelStyle: TextStyle(
-                          color: Colors.grey[700]), // Label text color
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 10.0,
-                          horizontal:
-                              10.0), // Adjusts padding for the dropdown content
-                      border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(10.0), // Rounded corners
-                        borderSide: const BorderSide(
-                          color: Colors.grey, // Border color when not focused
-                          width: 1.0, // Border thickness
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors
-                              .grey, // Border color when enabled but not focused
-                          width: 1.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.deepPurple, // Border color when focused
-                          width: 1.0,
-                        ),
+        child: Form(
+          key: _formKey, // Form key to manage validation
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Select a Venue',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
                     ),
-                    items: bookingProvider.venues.map((String venue) {
-                      return DropdownMenuItem<String>(
-                        value: venue,
-                        child: Text(venue),
-                      );
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      bookingProvider.setVenue(newValue!);
-                    },
-                  ),
-                  const SizedBox(height: 23),
-
-                  const Text(
-                    'Information Detail',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 20),
-
-                  CustomTextfield(
-                    label: 'First Name',
-                    controller: bookingProvider.firstNameController,
-                  ),
-                  const SizedBox(height: 20),
-
-                  CustomTextfield(
-                    label: 'Last Name',
-                    controller: bookingProvider.lastNameController,
-                  ),
-                  const SizedBox(height: 20),
-
-                  CustomTextfield(
-                    label: 'Email',
-                    controller: bookingProvider.emailController,
-                  ),
-                  const SizedBox(height: 20),
-
-                  CustomTextfield(
-                    label: 'Notes',
-                    controller: bookingProvider.notesController,
-                  ),
-                  const SizedBox(height: 20),
-                  const SizedBox(height: 50),
-
-                  Button(
-                    width: double.infinity,
-                    title: 'Make a Reservation',
-                    disable: false,
-                    onPressed: () async {
-                      try {
-                        // Make the reservation and get the response data
-                        var reservationData =
-                            await ApiService().makeReservation(
-                          date: bookingProvider.dateController.text,
-                          time: bookingProvider.timeController.text,
-                          venue: bookingProvider.selectedVenue!,
-                          firstName: bookingProvider.firstNameController.text,
-                          lastName: bookingProvider.lastNameController.text,
-                          email: bookingProvider.emailController.text,
-                          notes: bookingProvider.notesController.text,
+                    const SizedBox(height: 20),
+                    DropdownButtonFormField<String>(
+                      value: bookingProvider.selectedVenue,
+                      decoration: InputDecoration(
+                        labelText: 'Venue',
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 10.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      items: bookingProvider.venues.map((String venue) {
+                        return DropdownMenuItem<String>(
+                          value: venue,
+                          child: Text(venue),
                         );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        bookingProvider.setVenue(newValue!);
+                      },
+                      validator: (value) => value == null
+                          ? 'Please select a venue' // Venue validation
+                          : null,
+                    ),
+                    const SizedBox(height: 23),
 
-                        bookingProvider.clearFields();
+                    const Text(
+                      'Information Detail',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
 
-                        // Navigate to the ReservationDetailsPage after successful booking
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SuccessBooked(
-                              reservationData: reservationData,
-                            ),
-                          ),
-                        );
-                      } catch (e) {
-                        // Show an error message if something goes wrong
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Error: $e')),
-                        );
-                      }
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+                    CustomTextfield(
+                      label: 'First Name',
+                      controller: bookingProvider.firstNameController,
+                      validator: (value) =>
+                          value!.isEmpty ? '*First Name is required' : null,
+                    ),
+                    const SizedBox(height: 20),
+
+                    CustomTextfield(
+                      label: 'Last Name',
+                      controller: bookingProvider.lastNameController,
+                      validator: (value) =>
+                          value!.isEmpty ? '*Last Name is required' : null,
+                    ),
+                    const SizedBox(height: 20),
+
+                    CustomTextfield(
+                      label: 'Email',
+                      controller: bookingProvider.emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return '*Email is required';
+                        }
+                        // Email validation using a RegExp pattern
+                        String emailPattern =
+                            r'^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$';
+                        RegExp regex = RegExp(emailPattern);
+                        if (!regex.hasMatch(value)) {
+                          return '*Please enter a valid email address'; // If email doesn't match the pattern
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+
+                    CustomTextfield(
+                      label: 'Notes',
+                      controller: bookingProvider.notesController,
+                      validator: (value) =>
+                          value!.isEmpty ? '*Notes are required' : null,
+                    ),
+                    const SizedBox(height: 50),
+
+                    Button(
+                      width: double.infinity,
+                      title: 'Make a Reservation',
+                      disable: false,
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          // Form is valid, try to make the reservation
+                          try {
+                            var reservationData =
+                                await ApiService().makeReservation(
+                              date: bookingProvider.dateController.text,
+                              time: bookingProvider.timeController.text,
+                              venue: bookingProvider.selectedVenue!,
+                              firstName: bookingProvider.firstNameController.text,
+                              lastName: bookingProvider.lastNameController.text,
+                              email: bookingProvider.emailController.text,
+                              notes: bookingProvider.notesController.text,
+                            );
+
+                            bookingProvider.clearFields();
+
+                            // Navigate to SuccessBooked page after successful booking
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SuccessBooked(
+                                  reservationData: reservationData,
+                                ),
+                              ),
+                            );
+                          } catch (e) {
+                            // Show an error message if something goes wrong
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Error: $e')),
+                            );
+                          }
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
